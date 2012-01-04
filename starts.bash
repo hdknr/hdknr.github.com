d3a4ab3fc5cbe2591a5344cb71d7e4e1811a14ca
hdknr <gmail@hdknr.com>
2012-01-04 11:22:44 +0900

    Updates....

--- starts.bash
+++ starts.bash
@@ -4,6 +4,7 @@ pip install jinja2
 pip install -e git+ssh://git@github.com/hdknr/jinjatag.git#egg=jinjatag
 pip install -e hg+ssh://hg@bitbucket.org/hdknr/sphinx-dev#egg=sphinx
 pip install -e hg+ssh://hg@bitbucket.org/hdknr/ve#egg=ve
+pip install -e git+https://github.com/michaeljones/sphinx-to-github.git#egg=sphinxtogithub
 #
 hg clone ssh://hg@bitbucket.org/hdknr/hdknr.bitbucket.org ../hdknr.bitbucket.org
 #git clone git@github.com:hdknr/hdknr.github.com.git
