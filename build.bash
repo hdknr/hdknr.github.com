3244c9623e9fa807138f3b284f68fdc60d529dd1
hdknr <gmail@hdknr.com>
2012-01-05 15:34:56 +0900

    Updates....

--- build.bash
+++ build.bash
@@ -2,6 +2,9 @@
 
 for i in src/* ; do
     echo "$i > docs/`basename $i`";
+    if [ ! -d docs/`basename $i` ] ; then
+        mkdir -p docs/`basename $i`;
+    fi;
     cp -r $i/build/html/* docs/`basename $i`;
     git add docs/`basename $i`;
 done
