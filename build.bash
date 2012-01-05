9b930f434f4886f0a6544bd29f1efbd424e05985
hdknr <gmail@hdknr.com>
2012-01-06 06:04:48 +0900

    Show links to my docs/jQuery sub content loader

--- build.bash
+++ build.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+cat > docs.html <<EOF
+<div id="sphinx-docs">
+ <ul>
+EOF
+
 for i in src/* ; do
+    DOC=`basename $i`;
     echo "$i > docs/`basename $i`";
-    if [ ! -d docs/`basename $i` ] ; then
-        mkdir -p docs/`basename $i`;
+    if [ ! -d docs/$DOC ] ; then
+        mkdir -p docs/$DOC ;
     fi;
-    cp -r $i/build/html/* docs/`basename $i`;
-    git add docs/`basename $i`;
+    cp -r $i/build/html/* docs/$DOC;
+    git add docs/$DOC ;
+    cat >> docs.html << EOF
+    <li><a href="docs/$DOC/">$DOC</a></li>
+EOF
 done
-git commit -a -m "Updates...."
+cat >> docs.html <<EOF
+  </ul>
+</div>
+EOF
+
+#git commit -a -m "Updates...."
 #git push
