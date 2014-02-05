482f96221e9cbc41bd62f80621fa3ef6e792b7b1
hdknr <gmail@hdknr.com>
2014-02-05 20:02:41 +0900

    sentory for Python

---
+++ src/python/source/tools/sentry/django_create_project.bash
@@ -0,0 +1,14 @@
+(tact)$ mkdir web
+(tact)$ django-admin.py startproject app web
+
+(tact)$ tree web
+web
+├── app
+│   ├── __init__.py
+│   ├── settings.py
+│   ├── urls.py
+│   └── wsgi.py
+└── manage.py
+
+1 directory, 5 files
+
